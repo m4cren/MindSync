bf2f24d99e26047765642fee9ca34ca7a1295256
m4cren <renrenofficial@gmail.com>
2025-07-27 16:15:56 +0800

    Set up nextjs, firebase and Redux

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SyncLog",
+  description:
+    "A refined, personal habit and expense tracker inspired by Notion — built to serve real purpose and daily growth.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable}  antialiased`}>{children}</body>
+    </html>
+  );
+}
